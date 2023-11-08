0ce1401db08707cdda2feec988c81d28a54a41a6
giangnguyen <giangducnguyen.13@gmail.com>
2023-11-08 21:22:38 +0700

    assignment 02/11/2023

--- app/src/main/java/com/example/roll_dice/MainActivity.kt
+++ app/src/main/java/com/example/roll_dice/MainActivity.kt
@@ -3,9 +3,10 @@ package com.example.roll_dice
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.widget.Toast
-import androidx.viewbinding.ViewBinding
+import com.example.roll_dice.adapter.VerticalAdapter
 import com.example.roll_dice.databinding.ActivityMainBinding
+import com.example.roll_dice.model.ItemHorizontal
+import com.example.roll_dice.model.ItemVertical
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,28 +16,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = getViewBinding(layoutInflater)
         setContentView(binding.root)
-
-        try {
-            binding.button.setOnClickListener {
-                val drawableResource = when (rollDice()) {
-                    1 -> R.drawable.dice_1
-                    2 -> R.drawable.dice_2
-                    3 -> R.drawable.dice_3
-                    4 -> R.drawable.dice_4
-                    5 -> R.drawable.dice_5
-                    else -> R.drawable.dice_6
-                }
-
-                binding.img.setImageResource(drawableResource)
+        var items = mutableListOf<ItemVertical>()
+        for (i in 1..30){
+            var itemHorizontals = mutableListOf<ItemHorizontal>()
+            for(i in 1..20){
+                itemHorizontals.add(
+                    ItemHorizontal(
+                        "Game $i",
+                        "4.5"
+                    )
+                )
             }
-        } catch (e: Exception){
-            e.printStackTrace()
+            items.add(
+                ItemVertical(
+                "Popular App $i",
+                    itemHorizontals
+                )
+            )
         }
+
+        val adapter = VerticalAdapter(items)
+        binding.rcvVertical.adapter = adapter
     }
 
-    private fun rollDice(): Int {
-        return (1..6).random()
-    }
 
     private fun getViewBinding(inflater: LayoutInflater): ActivityMainBinding {
         return ActivityMainBinding.inflate(inflater)
